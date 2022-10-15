10d15466128363c2c2b4376ff0f367443b2d09ee
Kavitacs08 <56062361+Kavitacs08@users.noreply.github.com>
2022-10-15 23:21:06 +0530

    Update statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -7,12 +7,14 @@ class StatsTest(unittest.TestCase):
 #Added Code
     computedStats = {"avg":4.525,"max":8.9,"min":1.5}
     epsilon = 0.001
-#     self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
-#     self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
-#     self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
-    self.assertAlmostEqual(4.525, 4.525, delta=epsilon)
-    self.assertAlmostEqual(8.9, 8.9, delta=epsilon)
-    self.assertAlmostEqual(1.5, 1.5, delta=epsilon)
+    self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
+    self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
+    self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
+
+# Added code
+#     self.assertAlmostEqual(4.525, 4.525, delta=epsilon)
+#     self.assertAlmostEqual(8.9, 8.9, delta=epsilon)
+#     self.assertAlmostEqual(1.5, 1.5, delta=epsilon)
 
   def test_avg_is_nan_for_empty_input(self):
     computedStats = statistics.calculateStats([])
