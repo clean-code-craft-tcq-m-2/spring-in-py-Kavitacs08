efaccf22cce84e7d466d6608a7e7bb4a1936b72a
Kavitacs08 <56062361+Kavitacs08@users.noreply.github.com>
2022-10-15 23:34:23 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -4,7 +4,7 @@ from __future__ import division
 def calculateStats(numbers):
   min_val = min(numbers)
   max_val = max(numbers)
-  avg = 0 if len(somelist) == 0 else sum(numbers)/len(numbers)
+  avg = 0 if len(numbers) == 0 else sum(numbers)/len(numbers)
   return {"avg" : avg,"min": min_val,"max": max_val}
 
 #Added code 
