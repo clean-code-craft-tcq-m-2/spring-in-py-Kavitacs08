b95b127a3019683c624f0f6e5dabe5442ea63ece
Kavitacs08 <56062361+Kavitacs08@users.noreply.github.com>
2022-10-15 23:52:22 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -2,13 +2,19 @@
 from __future__ import division
 
 def calculateStats(numbers):
-  min_val = min(numbers)
-  print("MIN",min_val)
-  max_val = max(numbers)
-  print("MAX",max_val)
-  avg = 0 if len(numbers) == 0 else sum(numbers)/len(numbers)
-  print("AVG",avg)
-  dict_min_max = {"avg" : avg,"min": min_val,"max": max_val}
+  if numbers.isnan():
+    min_val = nan
+    max_val = nan
+    avg = nan
+    dict_min_max = {"avg" : avg,"min": min_val,"max": max_val}
+  else:
+    min_val = min(numbers)
+    print("MIN",min_val)
+    max_val = max(numbers)
+    print("MAX",max_val)
+    avg = 0 if len(numbers) == 0 else sum(numbers)/len(numbers)
+    print("AVG",avg)
+    dict_min_max = {"avg" : avg,"min": min_val,"max": max_val}
   print("dict_min_max",dict_min_max)
   return dict_min_max
 
